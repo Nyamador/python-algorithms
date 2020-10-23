4d54751a7239eb9e169733697af5af4a39dd2ae5
Nyamador <dselasi4u@gmail.com>
2020-10-23 06:50:52 +0000

    First Commit

--- BinarySearch.py
+++ BinarySearch.py
@@ -5,6 +5,9 @@
     Algorithm Type: Divide and Conquer
 """
 
+
+# Recursive 
+
 def binarySearch(array, target):
     return binarySearchHelper(array, target, 0, len(array) - 1)
 
@@ -12,3 +15,10 @@ def binarySearchHelper(array, target, left, right):
     if left > right:
         return -1
     middle = (left + right) // 2
+    potentialMatch = array[middle]
+    if target == potentialMatch:
+        return middle
+    elif target < potentialMatch:
+        return binarySearchHelper(array, target, left, middle - 1)
+    else:
+        return binarySearchHelper(array, target, middle + 1 , right)
